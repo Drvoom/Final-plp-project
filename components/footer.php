982290569ea00d83acb1089fd6abafbdd62a3d9e
Garba Ifeoluwa Joshua <142104585+Drvoom@users.noreply.github.com>
2024-12-31 20:59:49 +0100

    Update footer.php
    done

--- components/footer.php
+++ components/footer.php
@@ -22,49 +22,47 @@
                 </div>
              </div>
           </div>
-          <div class="row g-5">
-             <div class="col-lg-3 col-md-6">
-                <div class="footer-item">
-                   <h4 class="text-light mb-3">Why People Like us!</h4>
-                   <p class="mb-4">typesetting, remaining essentially unchanged. It
-                      was
-                      popularised in the 1960s with the like Aldus PageMaker
-                      including of Lorem Ipsum.</p>
-                   <a href="" class="btn border-secondary py-2 px-4 rounded-pill text-primary">Read
-                      More</a>
-                </div>
-             </div>
-             <div class="col-lg-3 col-md-6">
-                <div class="d-flex flex-column text-start footer-item">
-                   <a class="btn-link" href="">About Us</a>
-                   <a class="btn-link" href="">Contact Us</a>
-                   <a class="btn-link" href="">Privacy Policy</a>
-                   <a class="btn-link" href="">Terms & Condition</a>
-                   <a class="btn-link" href="">Return Policy</a>
-                   <a class="btn-link" href="">FAQs & Help</a>
-                </div>
-             </div>
-             <div class="col-lg-3 col-md-6">
-                <div class="d-flex flex-column text-start footer-item">
-                   <h4 class="text-light mb-3">Account</h4>
-                   <a class="btn-link" href="">My Account</a>
-                   <a class="btn-link" href="">Shop details</a>
-                   <a class="btn-link" href="">Shopping Cart</a>
-                   <a class="btn-link" href="">Wishlist</a>
-                   <a class="btn-link" href="">Order History</a>
-                   <a class="btn-link" href="">International Orders</a>
-                </div>
-             </div>
-             <div class="col-lg-3 col-md-6">
-                <div class="footer-item">
-                   <h4 class="text-light mb-3">Contact</h4>
-                   <p>Address: 1429 Netus Rd, NY 48247</p>
-                   <p>Email: Example@gmail.com</p>
-                   <p>Phone: +0123 4567 8910</p>
-                   <img src="img/payment.png" class="img-fluid" alt="">
-                </div>
-             </div>
-          </div>
+      
+
+<div class="row g-5">
+<div class="col-lg-3 col-md-6">
+<div class="footer-item">
+<h4 class="text-light mb-3">Why Choose Our Telemedicine Platform?</h4>
+<p class="mb-4">Experience convenient, high-quality healthcare from the comfort of your own home. Our platform connects you with board-certified physicians and healthcare professionals.</p>
+<a href="#" class="btn border-secondary py-2 px-4 rounded-pill text-primary">Learn More</a>
+</div>
+</div>
+<div class="col-lg-3 col-md-6">
+<div class="d-flex flex-column text-start footer-item">
+<h4 class="text-light mb-3">Quick Links</h4>
+<a class="btn-link" href="#">About Us</a>
+<a class="btn-link" href="#">Contact Us</a>
+<a class="btn-link" href="#">Privacy Policy</a>
+<a class="btn-link" href="#">Terms & Conditions</a>
+<a class="btn-link" href="#">FAQs & Help</a>
+</div>
+</div>
+<div class="col-lg-3 col-md-6">
+<div class="d-flex flex-column text-start footer-item">
+<h4 class="text-light mb-3">Patient Resources</h4>
+<a class="btn-link" href="#">My Account</a>
+<a class="btn-link" href="#">Appointment Scheduling</a>
+<a class="btn-link" href="#">Prescription Refills</a>
+<a class="btn-link" href="#">Medical Records</a>
+<a class="btn-link" href="#">Patient Portal</a>
+</div>
+</div>
+<div class="col-lg-3 col-md-6">
+<div class="footer-item">
+<h4 class="text-light mb-3">Contact Us</h4>
+<p>Phone: 1-800-MED-CARE (1-800-633-2273)</p>
+<p>Email: mailto:info@telemedicineplatform.com</p>
+<p>Address: 123 Main St, Anytown, Nigeria 12345</p>
+<img src="img/payment.png" class="img-fluid" alt="">
+</div>
+</div>
+</div>
+
        </div>
     </div>
     <!-- Footer End -->
@@ -81,7 +79,7 @@
              <div class="col-md-6 my-auto text-center text-md-end text-white">
                 <!--/*** This template is free as long as you keep the below author’s credit link/attribution link/backlink. ***/-->
                 <!--/*** If you'd like to use the template without the below author’s credit link/attribution link/backlink, ***/-->
-                <!--/*** you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". ***/-->
+                <!--/*** you can purchase the Credit Removal License from "https:/". ***/-->
                 Designed By <a class="border-bottom" href="/">Drvoom</a>
              </div>
           </div>
